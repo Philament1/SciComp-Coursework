f8c2e1b86384ad33d0f62a9fe23921d3bab86f54
Phil Yu <phil.yu21@imperial.ac.uk>
2023-11-01 16:01:15 +0000

    qwe

--- Project 1/project1.py
+++ Project 1/project1.py
@@ -17,13 +17,13 @@ def part1(Xin,istar):
     """
     X = Xin.copy() 
     for i,x in enumerate(X[1:],1):
-        if i<=istar:    #  Insertion sort
+        if i<=istar:
             ind = 0
             for j in range(i-1,-1,-1):
                 if x>=X[j]:
                     ind = j+1
                     break                   
-        else:           #  Binary insertion sort
+        else:
             a = 0
             b = i-1
             while a <= b:
@@ -52,6 +52,9 @@ def part1_time(inputs=None):
     import time
     
     def sortedlist_timer(N, istar, ascending=True):
+        """
+        Timer for a sorted list of length N (can vary istar and ascending/descending)
+        """
         
         if ascending:
             input = list(range(N))
@@ -59,88 +62,74 @@ def part1_time(inputs=None):
             input = list(range(N-1, -1, -1))
         
         t1 = time.time()
-        part1(input, istar)
+        for i in range(10):
+            part1(input, istar)
         t2 = time.time()
 
-        return t2-t1
+        return (t2-t1)/10
     
     def noduplicateslist_timer(N, istar):
+        """
+        Timer for a shuffled list with no duplicate integers of length N (can vary istar)
+        """
 
         input = list(range(N))
         np.random.shuffle(input)
 
         t1 = time.time()
-        part1(input, istar)
+        for i in range(10):
+            part1(input, istar)
         t2 = time.time()
 
-        return t2-t1
+        return (t2-t1)/10
     
     def randomlist_timer(N, istar, sample_range):
+        """
+        Timer for random list of length N sampled from 0 to sample_range (can vary istar)
+        """
 
         input = np.random.randint(0, sample_range, size=N)
 
         t1 = time.time()
-        part1(input, istar)
+        for i in range(10):
+            part1(input, istar)
         t2 = time.time()
 
-        return t2-t1
-        
-    N_list = np.logspace(1, 4, num=8, dtype=int)
-    sample_ranges = []
-    istar_vals = ["0", "N//2", "N-1"]
+        return (t2-t1)/10
+    
+    N_list = np.logspace(1, 4, num=10, dtype=int)    #   N values
+    istar_vals = ["0", "N//2", "N-1"]       #  istar cases
 
     mm = len(N_list)
     nn = len(istar_vals)
 
-    times_asc = np.zeros((mm,nn))
-    times_desc = np.zeros((mm,nn))
-    times_nodup = np.zeros((mm,nn))
-    times_random1 = np.zeros((mm,nn))
-    times_random2 = np.zeros((mm,nn))
+    #   Timings where each row is a different N value, and columns are different istar cases:
+    times_asc = np.zeros((mm,nn))       #   Ascending sorted list
+    times_desc = np.zeros((mm,nn))      #   Descending sorted list
+    times_nodup = np.zeros((mm,nn))     #   Random list with no duplicate values
+    times_random = np.zeros((mm,nn))    #   Random list sampled from 0 to 2*N
 
     for j, N in enumerate(N_list):
         istars = [0, N//2, N-1]
         for k, istar in enumerate(istars):
             t1 = sortedlist_timer(N, istar, ascending=True)
-            
+            print(N, istar_vals[k], "ascending", t1)
+            times_asc[j,k] = t1
 
             t2 = sortedlist_timer(N, istar, ascending=False)
-            
-            
+            print(N, istar_vals[k], "descending", t2)
+            times_desc[j,k] = t2
 
             t3 = noduplicateslist_timer(N, istar)
-            
-            
-
-            t4 = randomlist_timer(N, istar, N)
-            
-            
-
-            t5 = randomlist_timer(N, istar, N//4)
-            
-
-            times_asc[j,k] = t1
-            times_desc[j,k] = t2
-            times_nodup[j,k] = t3
-            times_random1[j,k] = t4
-            times_random2[j,k] = t5
-
-            print(N, istar_vals[k], "ascending", t1)
-            print(N, istar_vals[k], "descending", t2)
             print(N, istar_vals[k], "no duplicates", t3)
-            print(N, istar_vals[k], "random1", t4)
-            print(N, istar_vals[k], "random2", t5)
+            times_nodup[j,k] = t3
+
+            t4 = randomlist_timer(N, istar, 2*N)
+            print(N, istar_vals[k], "random", t4)
+            times_random[j,k] = t4
                 
-    fig, ax = plt.subplots(1,2)
-    """
-    ax[k].set_title(f"istar = {istar_vals[k]}")
-    ax[k].plot(N_list, times_asc[:,k], label="Ascending")
-    ax[k].plot(N_list, times_desc[:,k], label="Descending")
-    ax[k].plot(N_list, times_nodup[:,k], label="No duplicates")
-    ax[k].plot(N_list, times_random1[:,k], label="Random (range N)")
-    ax[k].plot(N_list, times_random2[:,k], label="Random (range N//4)")
-    ax[k].legend()
-    """
+    fig, ax = plt.subplots(1,4)
+
     ax[0].set_title("Ascending")
     for k in range(nn):
         ax[0].plot(N_list, times_asc[:,k], label=istar_vals[k])
@@ -150,6 +139,16 @@ def part1_time(inputs=None):
     for k in range(nn):
         ax[1].plot(N_list, times_desc[:,k], label=istar_vals[k])
         ax[1].legend()
+
+    ax[2].set_title("Random No Duplicates")
+    for k in range(nn):
+        ax[2].plot(N_list, times_nodup[:,k], label=istar_vals[k])
+        ax[2].legend()
+
+    ax[3].set_title("Random 0 to 2N")
+    for k in range(nn):
+        ax[3].plot(N_list, times_random[:,k], label=istar_vals[k])
+        ax[3].legend()
 
     plt.show()
 
