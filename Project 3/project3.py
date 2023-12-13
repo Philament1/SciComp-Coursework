cef19825d2d0f564a4c6b4fb7abffe525f18549f
Phil Yu <phil.yu21@imperial.ac.uk>
2023-12-13 17:04:45 +0000

    aiosdioasdkl

--- Project 3/project3.py
+++ Project 3/project3.py
@@ -7,6 +7,7 @@ from scipy.integrate import solve_ivp
 #use scipy as needed
 import scipy
 from scipy import sparse as sp
+from scipy import fft
 import time
 
 #===== Code for Part 1=====#
@@ -21,12 +22,20 @@ def plot_field(lat,lon,u,time,levels=20):
     time: time at which wind speed will be plotted (index between 0 and 364)
     levels: number of contour levels in plot
     """
+    # plt.figure()
+    # plt.contourf(lon,lat,u[time,:,:],levels)
+    # plt.axis('equal')
+    # plt.grid()
+    # plt.xlabel('longitude')
+    # plt.ylabel('latitude')
+
     plt.figure()
-    plt.contourf(lon,lat,u[time,:,:],levels)
+    contour = plt.contourf(lon,lat,u[time,:,:],levels)
     plt.axis('equal')
     plt.grid()
     plt.xlabel('longitude')
     plt.ylabel('latitude')
+    plt.colorbar(contour)
 
     return None
 
@@ -41,7 +50,7 @@ def plot_field2(lat,lon,u, time_points,levels=20):
         ax[i].set_ylabel('latitude')
 
 
-def part1():#add input if needed
+def part1(time_as_datapoints = True):#add input if needed
     """
     Code for part 1
     """ 
@@ -53,59 +62,126 @@ def part1():#add input if needed
 
     #Add code here 
 
-    L, M, N = u.shape # L = 365, M = 16, N = 144
-
-    A = u.reshape(L, M * N).T
-    A_bar = np.mean(A, axis=1)
-    A -= A_bar[:, None]  #   A ((M x N) x L) is u unrolled into L columns of length M x N vectors
-
-    U, S, WT = np.linalg.svd(A, full_matrices=False)     #   Getting U ((M x N) x L) and S (L)
-    rank = S[S>1e-11].size
-    print(f'rank A: {rank}')  
-
-    k = rank      #  How many principal components?
-    
-    Atilde = np.dot(U[:, :k].T, A)     #   Atilde (k x N) our new variables 
-
-    #   Reconstruction
-
-    A_PCA = np.dot(U[:, :k], Atilde)     #   A_pca ((M x N) x L) reconstruction of A
-    A_PCA += A_bar[:, None]
-    utilde = A_PCA.T.reshape(L, M, N)
-
     #   PLOTS
-
     plot_field2(lat, lon, u, np.linspace(0, 364, 5, dtype=int))
 
-    #   Singular values
-    plt.figure()
-    plt.semilogy(S[:-1])
+    L, M, N = u.shape # L = 365, M = 16, N = 144
 
-    #   Spatial patterns
-    # plt.figure()
-    # plt.imshow(U[:,0].reshape((M, N)), cmap='bwr', interpolation='nearest')
-    plot_field2(lat, lon, U.T.reshape((L, M, N)), np.arange(0, 5))
+    def PCA(A, k=0):
+        """
+        Run PCA on A
+        
+        Input:
+        A: p x N row-centered matrix, where p is number of features/attributes, N is number of data points
+        k: number of principal components (default=0 uses rank of A)
+        Output:
+        PC: k x p matrix, each row is a principal component
+        S: length k array, singular values
+        Atilde: k x N matrix, transformed data
+        """
+        U, S, WT = np.linalg.svd(A, full_matrices=False)     #   Getting U ((M x N) x L) and S (L)
+        rank = S[S>1e-11].size
+        print(f'rank A: {rank}')  
 
-    #   Temporal trends
-    plt.figure()
-    plt.plot(Atilde[0])
-    plt.plot(Atilde[1])
+        if k == 0:
+            k = rank
 
-    #   Reconstruction
-    plot_field2(lat, lon, utilde, np.linspace(0, 364, 5, dtype=int))
-
-    #   PC2 vs PC1
-    plt.figure()
-    plt.scatter(Atilde[0], Atilde[1])
+        PC = U[:, :k].T  #  Principal components
+        
+        Atilde = PC @ A     #   New variables
+        return S[:k], PC, Atilde
     
-    # fig, axs = plt.subplots(3, 3, figsize = (15, 15))
-    # for i, ax in enumerate(axs.reshape(-1)):
-    #     ax.contourf(lon,lat,utilde[i,:,:], 20)
-    #     ax.axis('equal')
-    #     ax.grid()
+    if time_as_datapoints:
+        '''CASE 1: TIME AS DATA POINTS'''
 
+        A = u.reshape(L, M * N).T  # A ((M x N) x L) is u unrolled into L columns of length M x N vectors
+        A_bar = np.mean(A, axis=1)
+        A -= A_bar[:, None]   # Centering the rows of A
+        S, PC, Atilde = PCA(A)
+
+        # #   Reconstruction
+        # A_red = PC.T @ Atilde     #   A_red ((M x N) x L) reconstruction of A
+        # A_red += A_bar[:, None]
+        # utilde = A_red.T.reshape(L, M, N)
+        # plot_field2(lat, lon, utilde, np.linspace(0, 364, 5, dtype=int))
+
+        #   Singular values
+        plt.figure()
+        plt.title('Singular values')
+        plt.semilogy(S)
+
+        #   Principal components (along space)
+        # plt.figure()
+        # plt.imshow(U[:,0].reshape((M, N)), cmap='bwr', interpolation='nearest')
+        plot_field2(lat, lon, PC.reshape((364, M, N)), np.arange(0, 5))
+
+        #   Temporal trends (plotting rows of Atilde)
+        plt.figure()
+        plt.plot(Atilde[0], label = 'PC1')
+        plt.plot(Atilde[1], label = 'PC2')
+        plt.legend()
+
+        #   Fourier on temporal trends
+        plt.figure()
+        mode = np.arange(-L//2, L//2)
+        spect1 = abs(fft.fftshift(fft.fft(Atilde[0])))
+        spect2 = abs(fft.fftshift(fft.fft(Atilde[1])))
+        plt.semilogy(mode, spect1)
+        plt.semilogy(mode, spect2)
+
+        plt.figure()
+        freq, welch1 = scipy.signal.welch(Atilde[0])
+        plt.semilogy(freq, welch1)
+
+        #   Explained variance
+        cum_var_rat = np.cumsum(S**2/np.sum(S**2))
+
+        plt.figure()
+        plt.plot(cum_var_rat[:50])
+        plt.axhline(y=1, color='r')
+        plt.axhline(y=0.8, color='k', linestyle='--')
+
+        #   PC2 vs PC1
+        # plt.figure()
+        # plt.scatter(Atilde[0], Atilde[1])
+
+        plt.show()
     
-    plt.show()
+    else:
+        '''CASE 2: LOCATIONS AS DATA POINTS'''
+
+        A = u.reshape(L, M * N)  # A (L x (M x N)) is u unrolled into M x N columns of length L vectors
+
+        A_bar = np.mean(A, axis=1)
+        A -= A_bar[:, None]   # Centering the rows of A
+        S, PC, Atilde = PCA(A)
+
+        #   Singular values
+        plt.figure()
+        plt.title('Singular values')
+        plt.semilogy(S)
+
+        #   Explained variance
+        cum_var_rat = np.cumsum(S**2/np.sum(S**2))
+        
+        plt.figure()
+        plt.plot(cum_var_rat[:50])
+        plt.axhline(y=1, color='r')
+        plt.axhline(y=0.8, color='k', linestyle='--')
+
+        #   Principal components (along time)  # Note they are negative
+        plt.figure()
+        plt.plot(PC[0], label = 'PC1')
+        plt.plot(PC[1], label = 'PC2')
+        plt.legend()
+
+        #   Spatial patterns (plotting rows of Atilde)
+        # plt.figure()
+        # plt.imshow(U[:,0].reshape((M, N)), cmap='bwr', interpolation='nearest')
+        plot_field(lat, lon, Atilde.reshape((L, M, N)), 0)
+
+        plt.show()
+
 
     return None #modify if needed
 
@@ -125,6 +201,7 @@ def part2(f,method=2):
 
     if method==1:
         fI = 0.5*(f[:-1,:]+f[1:,:])
+    
     else:
         #Coefficients for method 2
         alpha = 0.3
@@ -136,16 +213,16 @@ def part2(f,method=2):
 
         #add code here
 
-        #   Using sparse
-        A = sp.diags([[alpha]*(m-3) + [0], 1, [0] + [alpha]*(m-3)], [-1,0,1]).toarray()
+        AB = np.vstack([np.full(m-1, alpha), np.ones(m-1), np.full(m-1, alpha)])
+        AB[0, 0] = AB[0, 1] = 0
+        AB[-1, -1] = AB[-1, -2] = 0
 
         diag0 = [a_bc]+[a/2]*(m-3)+[b_bc]
         diag1 = [b/2]*(m-3)+[c_bc]
         diag2 = [0]*(m-4)+[d_bc]
-
         B = sp.diags([diag2, diag1, diag0, diag0[::-1], diag1[::-1], diag2[::-1]], [-2, -1,0,1,2, 3], shape=(m-1,m)).toarray()
 
-        fI = np.linalg.solve(A, B @ f)
+        fI = scipy.linalg.solve_banded((1,1), AB, B @ f)
 
     return fI #modify as needed
 
@@ -155,7 +232,7 @@ def part2_analyze():
     """
 
     #----- Code for generating grid, use/modify/discard as needed ----#
-    n,m = 100,100 #arbitrary grid sizes
+    n,m = 50,40 #arbitrary grid sizes
     x = np.linspace(0,1,n)
     y = np.linspace(0,1,m)
     xg,yg = np.meshgrid(x,y)
@@ -171,44 +248,54 @@ def part2_analyze():
         f = func(xg, yg)
         fI = func(xIg, yIg)
 
-        t = time.time()
-        fI1 = part2(f, method=1)
-        t1 = time.time()-t
+        #   Running and timing
+        N = 1000
+        t1 = 0
+        t2 = 0
+        for i in range(N):
+            t = time.time()
+            fI1 = part2(f, method=1)
+            t1 += time.time()-t
 
-        t = time.time()
-        fI2 = part2(f, method=2)
-        t2 = time.time()-t
+            t = time.time()
+            fI2 = part2(f, method=2)
+            t2 += time.time()-t
+        t1 /= N
+        t2 /= N
+        print(f'Time1: {t1}')
+        print(f'Time2: {t2}')
 
-        print(t1)
-        print(t2)
-
+        #   Accuracy and error plots
         error1 = abs(fI1-fI)
-        RMSE1 = np.mean(error1**2)
-        print(RMSE1)
+        MSE1 = np.mean(error1**2)
+        print(f'MSE1: {MSE1}')
         error2 = abs(fI2-fI)
-        RMSE2 = np.mean(error2**2)
-        print(RMSE2)
+        MSE2 = np.mean(error2**2)
+        print(f'MSE2: {MSE2}')
 
         fig, ax = plt.subplots(1, 3)
         im0 = ax[0].pcolormesh(xg, yg, f)
-        im1 = ax[1].pcolormesh(xIg, yIg, error1)
-        ax[2].pcolormesh(xIg, yIg, error2, vmin=im1.get_clim()[0], vmax=im1.get_clim()[1])
         fig.colorbar(im0)
-        fig.colorbar(im1, ax=[ax[1], ax[2]])
+        ax[0].set_aspect('equal')
+
+        im1 = ax[1].pcolormesh(xIg, yIg, error1)
+        #ax[2].pcolormesh(xIg, yIg, error2, vmin=im1.get_clim()[0], vmax=im1.get_clim()[1])
+        #fig.colorbar(im1, ax=[ax[1], ax[2]])
+
+        fig.colorbar(im1)
+        im2 = ax[2].pcolormesh(xIg, yIg, error2)
+        fig.colorbar(im2)
+
         plt.show()
 
-    testing_and_plots(lambda x, y: np.sin(2*np.pi*x) * np.cos(2*np.pi*y))
-    testing_and_plots(lambda x, y: np.sin(2*np.pi*x) * np.cos(2*np.pi*y) + np.where(y < 0.5, 0, 0.5))
-   
-    #   Plots
 
-    # fig, ax = plt.subplots(1, 4)
-    # ax[0].pcolormesh(xg, yg, f1)
-    # ax[1].pcolormesh(xIg, yIg, f1I)    
-    # ax[2].pcolormesh(xIg, yIg, f1I1)
-    # ax[3].pcolormesh(xIg, yIg, f1I2)  
-    # plt.show()
-
+    # testing_and_plots(lambda x, y: np.sin(2*np.pi*x) * np.cos(2*np.pi*y))
+    # testing_and_plots(lambda x, y: np.sin(2*np.pi*x) * np.cos(2*np.pi*y) + np.where(y < 0.5, 0, 0.5))
+    testing_and_plots(lambda x, y: (3/4) * np.exp(-((9*x-2)**2)/4 - ((9*y-2)**2)/4) + \
+                               (3/4) * np.exp(-((9*x+1)**2)/49 - ((9*y+1)**2)/10) + \
+                               (1/2) * np.exp(-((9*x-7)**2)/4 - ((9*y-3)**2)/4) - \
+                               (1/5) * np.exp(-((9*x-4)**2) - ((9*y-7)**2)))
+    
     return None #modify as needed
 
 
@@ -271,7 +358,10 @@ def part3_analyze(display = False):#add/remove input variables if needed
     beta = (25/np.pi)**2
     alpha = 1-2*beta
     b =-1.5
-    n = 4000
+    n = 1500 # default 4000
+
+    tf = 200
+    Nt = 800
 
     #Set initial conidition
     L = (n-1)/np.sqrt(beta)
@@ -297,32 +387,56 @@ def part3_analyze(display = False):#add/remove input variables if needed
 
     #Add code here
 
-    # FFT analysis
+    x = u[:,100:-99]
+    Nt_1, m = x.shape
+    times = np.linspace(0, tf, Nt_1)
 
-    Nt_1 = len(u)
-
-    Sf = np.fft.fftshift(np.fft.fft(u, axis=0))
-    f = np.fft.fftshift(np.fft.fftfreq(Nt_1, t[1]-t[0]))
-
-    # Plots
-    
+    #  Time plot
     plt.figure()
-    for i in range(n):
-        plt.plot(t, u[:,i])
+    for i in range(m):
+        plt.plot(t, x[:,i])
 
+    #  Space plot
     plt.figure()
-    Nt_1 = len(u)
     for j in range(Nt_1):
-        plt.plot(np.arange(n), u[j])
+        plt.plot(np.arange(100, n-99), x[j])
+
+    #  FFT analysis
+
+    Sf = np.fft.fftshift(np.fft.fft(x, axis=0))
+    f = np.fft.fftshift(np.fft.fftfreq(Nt_1, t[1]-t[0]))
 
     plt.figure()
     plt.plot(f, Sf[:, 0])
+    plt.figure()
+    plt.semilogy(f, Sf[:, 0])
+
+    #  Corr dimension
+
+    D = scipy.spatial.distance.pdist(x)
+    eps = np.logspace(-1, 2, 20)
+    C = [D[D < ep].size*2/(n*(n-1)) for ep in eps]
+
+    d, p_corr = np.polyfit(np.log(eps), np.log(C), 1)
+    print(d)
+
+    plt.figure()
+    plt.loglog(eps, C, marker = 'x')
+    plt.loglog(eps, np.exp(p_corr)*d*eps, linestyle='--')
+
+    #  Oscillation mapping
+    
+    dx = np.diff(x[:,0])
+    d2x  = dx[:-1]*dx[1:]
+    ind = np.argwhere(d2x<0)
+    
+    plt.figure()
+    plt.plot(x[:,0])
+
 
     plt.show()
 
-
-    return None #modify if needed
-
+    return None #modify if need
 
 def part3q2(x,c=1.0):
 
@@ -339,7 +453,9 @@ def part3q2(x,c=1.0):
     L = (n-1)/np.sqrt(beta)
     k = 40*np.pi/L
     a0 = np.linspace(0,L,n)
+    y0 = np.zeros(2*n)
     A0 = np.sqrt(1-k**2)*np.exp(1j*a0)
+
     y0[:n]=1+0.2*np.cos(4*k*a0)+0.3*np.sin(7*k*a0)+0.1*A0.real
 
     #Compute solution
@@ -361,4 +477,8 @@ if __name__=='__main__':
     x=None #Included so file can be imported
     #Add code here to call functions above if needed
 
-    part2_analyze()
+    # part1(time_as_datapoints=True)
+    # part2_analyze()
+    # part3_analyze()
+
+    print(part3q2())
